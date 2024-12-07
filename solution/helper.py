fc0075151e946c7b98849037fd93dd7818ecd757
Louis Charton <louis.charton@lbf.fraunhofer.de>
2024-12-07 17:04:08 +0100

    important paths in helper file

---
+++ solution/helper.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from pathlib import Path
+
+PROJECT_NAME: str = "Hackathon-2024"
+
+SCRIPT_PATH: Path = Path(os.path.abspath(sys.argv[0])).parent
+
+__PATH= SCRIPT_PATH
+while True:
+    __PATH = __PATH.parent
+    match __PATH.name: 
+        case "":
+            raise SystemError(f"Parent Folder <{PROJECT_NAME}> not found in parent directory")
+        case str(PROJECT_NAME):
+            PROJECT_PATH = __PATH
+            break
+        case _:
+            continue
+
+# print(PROJECT_PATH)
+SAMPLE_PATH: Path = PROJECT_PATH / "TEST_EXAMPLES"
+DATA_PATH: Path = PROJECT_PATH / "data/Rohdaten"

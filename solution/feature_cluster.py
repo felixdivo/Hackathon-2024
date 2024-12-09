3174ffcb9e447fa4feb2a71025c5cc6740135ae7
Louis Charton <louis.charton@lbf.fraunhofer.de>
2024-12-09 16:53:30 +0100

    preprocessing attempts

--- solution/feature_cluster.py
+++ solution/feature_cluster.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from scipy.ndimage import gaussian_filter
+    
 
 from typing import Iterable
 
@@ -22,22 +24,59 @@ def normalize(image: np.ndarray) -> np.ndarray:
 def grad(image: np.ndarray) -> np.ndarray:
     image = np.gradient(image)
     image = np.linalg.norm(image, axis=0)
-    return np.array(image/ np.max(image))
+    return (255 * image/ np.max(image)).astype(np.uint8)
 
 def std_dev(image: np.ndarray, win_size: tuple[int]) -> np.ndarray:
     mean_loc_sq = cv2.filter2D(image, -1, np.ones(win_size))
-    ret_val = np.sqrt((image-mean_loc_sq)**2)
-    return 255 * ret_val / np.max(ret_val)
+    ret_val: np.ndarray = np.sqrt((image-mean_loc_sq)**2)
+    return (255 * ret_val / np.max(ret_val)).astype(np.uint8)
 
 def abs_mean_dev(image: np.ndarray) -> np.ndarray:
     return np.abs(image - np.mean(image))
 
+def smooth(image: np.ndarray) -> np.ndarray:
+    kernel = np.ones((5,5))
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    # sigma = np.min(image.shape) / 100
+    # return gaussian_filter(image, sigma)
+    return image
+    
+def canny_edge(image: np.ndarray) -> np.ndarray:
+    clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(8,8))
+    ret = (255 * normalize(clahe.apply(image))).astype(np.uint8)
+    ret =  cv2.Canny(ret, 150, 150, L2gradient=True)
+    kernel = np.ones((5,5))
+    ret = cv2.morphologyEx(ret, cv2.MORPH_CLOSE, kernel)
+    # kernel = np.ones((1,1))
+    # ret = cv2.morphologyEx(ret, cv2.MORPH_ERODE, kernel)
+    return ret
+
+def fill_edge(edge: np.ndarray) -> np.ndarray:
+    _, edge = cv2.threshold(edge, 200, 255, cv2.THRESH_BINARY)        
+    contours, _ = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    largest_contour = max(contours, key=cv2.contourArea)
+    h, w = edge.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)  # Add a 1-pixel border around the image
+
+    # Seed point inside the largest contour
+    # (You can use cv2.boundingRect to find a seed point)
+    x, y, w, h = cv2.boundingRect(largest_contour)
+    seed_point = (x + w // 2, y + h // 2)
+
+    # Flood-fill the largest contour
+    filled_image = edge.copy()  # Make a copy of the binary image
+    cv2.floodFill(filled_image, mask, seed_point, 255)
+    return filled_image
+
 if __name__ == "__main__":
-    part_id =1
+    part_id = 4
     FILE_LIST = find_parts(DATA_PATH / f"part_{part_id}")
     NAME = FILE_LIST[3]
     print(f"LOADING: {NAME}")
     IMG = cv2.imread(NAME)
+    b_size = 20
+    IMG = cv2.copyMakeBorder(IMG, b_size, b_size, b_size, b_size, cv2.BORDER_CONSTANT)
 
     IMG_RGB = IMG.copy()
     IMG_LAB = cv2.cvtColor(IMG, cv2.COLOR_BGR2LAB)
@@ -49,9 +88,17 @@ if __name__ == "__main__":
     r, g, b, = cv2.split(IMG_RGB)
     
     win_size = (np.array(IMG_BW.shape) / 50).astype(np.int16)
+    
+    TEST = np.clip(
+        canny_edge(l_channel) +canny_edge(s) + canny_edge(v) + canny_edge(std_dev(x, win_size)) + canny_edge(std_dev(x, win_size)),#+ canny_edge(std_dev(r, win_size))+canny_edge(std_dev(g, win_size))+canny_edge(std_dev(b, win_size)), 
+        0, 255
+    )   
     show_imgs([
-        std_dev(x, win_size),
-        std_dev(y, win_size),
-        255* grad(IMG_BW),
-        255* normalize(l_channel)
+        # std_dev(x, win_size),
+        # std_dev(y, win_size),
+        # 255* grad(IMG_BW),
+        # 255* normalize(l_channel),
+        IMG_BW,
+        smooth(fill_edge(TEST)-TEST),
+        TEST
     ])
--- solution/preprocessing.py
+++ solution/preprocessing.py
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 import mpld3
         
 if __name__ == "__main__":
-    from helper import DATA_PATH
+    from helper import DATA_PATH, find_parts
 else:
-    from .helper import DATA_PATH
+    from .helper import DATA_PATH, find_parts
 
 class Image:
     def __init__(self, file_path: Path) -> Self:
+        print(f"LOADING: {file_path}")
         self.file_path: Path = file_path
         self._img: np.ndarray = cv2.imread(filename=file_path)
     	
@@ -22,130 +23,180 @@ class Image:
         input()
 
     def preprocessing_pipeline(self) -> None:
+        b_size = 20
+        self._img = cv2.copyMakeBorder(self._img, b_size, b_size, b_size, b_size, cv2.BORDER_CONSTANT)
         IMG = self._img.copy()
-        # self.__plot_rgb_ch_intensity_dist(IMG)
-        # IMG = self.__apply_threshold(IMG)
-        r_ch = IMG[:,:,1]
-        b_ch = IMG[:,:,0]
-        g_ch = IMG[:,:,2]
-        img_bw = cv2.cvtColor(IMG, cv2.COLOR_BGR2GRAY)
+    
+        from scipy.ndimage import gaussian_filter
+        IMG = gaussian_filter(IMG, 1, 0)
         
-        img_int_dist = Image.__rgb_ch_intensity_dist(img_bw)
-        b_int_dist = Image.__rgb_ch_intensity_dist(b_ch)
-        r_int_dist = Image.__rgb_ch_intensity_dist(r_ch)
-        g_int_dist = Image.__rgb_ch_intensity_dist(g_ch)
+        IMG_LAB = cv2.cvtColor(IMG, cv2.COLOR_BGR2LAB)
+        IMG_HSV = cv2.cvtColor(IMG, cv2.COLOR_BGR2HSV)
+        IMG_RGB = IMG.copy()
+        l_channel, x, y = cv2.split(IMG_LAB)
+        h, s, v = cv2.split(IMG_HSV)
+        r, g, b, = cv2.split(IMG_RGB)
+        r = self.__get_grad_norm_changes(r)
+        g = self.__get_grad_norm_changes(g)
+        b = self.__get_grad_norm_changes(b)
+        x = self.__get_grad_norm_changes(x)
+        y = self.__get_grad_norm_changes(y)
+        l_channel = self.__get_grad_norm_changes(l_channel)
+        hue = self.__get_grad_norm_changes(h)
+        s = self.__get_grad_norm_changes(s)
+        v = self.__get_grad_norm_changes(v)
+
+        IMG:np.ndarray = l_channel+v#+s+h
+        # IMG = gaussian_filter(IMG, 1, 0)
+        IMG = 255*IMG / np.max(IMG)
+        IMG = IMG.astype(np.uint8, casting="unsafe")
         
-        img_cum = Image.__rgb_channel_cumulative(img_int_dist)
-        b_cum = Image.__rgb_channel_cumulative(b_int_dist)
-        r_cum = Image.__rgb_channel_cumulative(r_int_dist)
-        g_cum = Image.__rgb_channel_cumulative(g_int_dist)
-
-        blue_thresh = len(b_cum[b_cum < 0.95])
-        red_thresh = len(r_cum[r_cum < 0.95])
-        green_thresh = len(g_cum[g_cum < 0.95])
-        print(blue_thresh, red_thresh, green_thresh)
-
-        r_ch[r_ch > red_thresh] = 0
-        g_ch[g_ch > green_thresh] = 0
-        b_ch[b_ch > blue_thresh] = 0
-        self._img = IMG
-
-    @staticmethod
-    def __apply_threshold(img: np.ndarray) -> np.ndarray:
-        img_int_dist, b_int_dist, r_int_dist, g_int_dist = Image.__rgb_ch_intensity_dist(img)
+        IMG = cv2.Canny(IMG, 10, 255, L2gradient=True)
+        IMG = gaussian_filter(IMG, .2, 0)
         
-        img[img > 160] = 0
-        img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        _, img_bw = cv2.threshold(img_bw, 30, 255, cv2.THRESH_BINARY)
-        # img[img_bw == 0] = 0 
-        # img_bw_processed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        return img_bw
-
-    @staticmethod
-    def __rgb_ch_intensity_dist(img_ch: np.ndarray, intensity_range: int = 1<<8) -> np.ndarray:
-        ret_val,_ = np.histogram(img_ch, bins=np.arange(intensity_range))
-        return ret_val
+        kernel = np.ones((5,5))
+        IMG = cv2.morphologyEx(IMG, cv2.MORPH_CLOSE, kernel)
         
-    @staticmethod
-    def __rgb_channel_cumulative(img_ch: np.ndarray) -> np.ndarray:
-        return np.asarray([
-            np.sum(img_ch[:ii+1]) for ii,_ in enumerate(img_ch)
-        ]) / np.sum(img_ch)
-
-    @staticmethod
-    def __plot_rgb_channel_cumulative(img: np.ndarray) -> None:
-        r_ch = img[:,:,1]
-        b_ch = img[:,:,0]
-        g_ch = img[:,:,2]
-        img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        IMG = np.clip(IMG*10, 0, 255)
+        _, IMG = cv2.threshold(IMG, 200, 255, cv2.THRESH_BINARY)        
         
-        img_int_dist = Image.__rgb_ch_intensity_dist(img_bw)
-        b_int_dist = Image.__rgb_ch_intensity_dist(b_ch)
-        r_int_dist = Image.__rgb_ch_intensity_dist(r_ch)
-        g_int_dist = Image.__rgb_ch_intensity_dist(g_ch)
-        
-        img_cum = Image.__rgb_channel_cumulative(img_int_dist)
-        b_cum = Image.__rgb_channel_cumulative(b_int_dist)
-        r_cum = Image.__rgb_channel_cumulative(r_int_dist)
-        g_cum = Image.__rgb_channel_cumulative(g_int_dist)
+        contours, _ = cv2.findContours(IMG, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        largest_contour = max(contours, key=cv2.contourArea)
+        h, w = IMG.shape[:2]
+        mask = np.zeros((h+2, w+2), np.uint8)  # Add a 1-pixel border around the image
 
-        plt.figure(figsize=(8,6))
-        plt.plot(img_cum, label="bw image intensity cumulative", color="y")
-        plt.plot(b_cum, label="blue channel intensity cumulative", color="b")
-        plt.plot(r_cum, label="red channel intensity cumulative", color="r")
-        plt.plot(g_cum, label="green channel intensity cumulative", color="g")
-        plt.legend()
-        plt.grid()
-        plt.xlabel("channel value")
-        plt.ylabel("N")
-        mpld3.show()
+        # Seed point inside the largest contour
+        # (You can use cv2.boundingRect to find a seed point)
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        seed_point = (x + w // 2, y + h // 2)
 
-    @staticmethod
-    def __plot_rgb_ch_intensity_dist(img: np.ndarray) -> None:
-        r_ch = img[:,:,1]
-        b_ch = img[:,:,0]
-        g_ch = img[:,:,2]
-        img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img_int_dist = Image.__rgb_ch_intensity_dist(img_bw)
-        b_int_dist = Image.__rgb_ch_intensity_dist(b_ch)
-        r_int_dist = Image.__rgb_ch_intensity_dist(r_ch)
-        g_int_dist = Image.__rgb_ch_intensity_dist(g_ch)
-
-        plt.figure(figsize=(8,6))
-        plt.plot(img_int_dist, label="bw image intensity dist", color="y")
-        plt.plot(r_int_dist, label="red channel intensity dist", color="r")
-        plt.plot(b_int_dist, label="blue channel intensity dist", color="b")
-        plt.plot(g_int_dist, label="green channel intensity dist", color="g")
-        plt.legend()
-        plt.grid()
-        plt.xlabel("channel value")
-        plt.ylabel("N")
-        mpld3.show()
-
-    @staticmethod
-    def __plot_rgb_channels(img: np.ndarray) -> None:
-        R = img.copy()
-        R[:,:,[0,2]] = 0
-        G = img.copy()
-        G[:,:,[0,1]] = 0
-        B = img.copy()
-        B[:,:,[1,2]] = 0
-        
-        cv2.imshow("test1", R)
-        cv2.imshow("test2", G)
-        cv2.imshow("test3", B)
-        cv2.imshow("img", img)
+        # Flood-fill the largest contour
+        filled_image = IMG.copy()  # Make a copy of the binary image
+        cv2.floodFill(filled_image, mask, seed_point, 255)
+        filled_image -= IMG
+        filled_image = cv2.cvtColor(filled_image, cv2.COLOR_GRAY2BGR)
+        IMG = cv2.cvtColor(IMG, cv2.COLOR_GRAY2BGR)
+        result = np.hstack((r, g, b, l_channel, hue, s, v))
+        cv2.imshow("R-G-B-Lch-Hue-Sat-Val", result)
+        cv2.imshow("image", self._img)
         cv2.waitKey(1)
         input()
+        
+        self._img = IMG
+        
 
+    @staticmethod
+    def __get_grad_norm_changes(img_1d: np.ndarray) -> np.ndarray:
+        # clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(32,32))
+        # img_1d = clahe.apply(img_1d)
+        
+        img_1d = np.gradient(img_1d)
+        img_1d = np.linalg.norm(img_1d, axis=0)
+        img_1d = np.array(img_1d/ np.max(img_1d))
+        
+        kernel = np.ones((3,3))
+        img_1d = cv2.morphologyEx(img_1d, cv2.MORPH_CLOSE, kernel)
+        return img_1d
 
 if __name__ == "__main__":
     try:
-        file_path: Path = DATA_PATH / "part_1/mask_20241202-170222-294.png"
-        IMG = Image(file_path)
+        part_id =13
+        FILE_LIST = find_parts(DATA_PATH / f"part_{part_id}")
+        IMG = Image(FILE_LIST[3])
         IMG.preprocessing_pipeline()
-        IMG.show()
+        # IMG.show()
     except Exception as E:
-        print(E)
+        raise E
     finally:
         cv2.destroyAllWindows()
+
+
+# IMG = enhanced_img
+        # img_bw = cv2.cvtColor(enhanced_img, cv2.COLOR_BGR2GRAY)
+        
+        # img_bw[img_bw == 0] = 1
+        # max_int = 255 * np.ones_like(img_bw)
+
+        # compression_mask = np.divide(max_int, img_bw)
+        # compression_mask[compression_mask == np.inf] = 0
+        # for ch in range(IMG.shape[2]):
+        #     IMG[:,:,ch] = np.multiply(IMG[:,:,ch], compression_mask)
+        
+        # img_bw_compressed = cv2.cvtColor(IMG, cv2.COLOR_BGR2GRAY)
+        # img_int_dist = Image.__rgb_ch_intensity_dist(img_bw_compressed)
+        
+        # from scipy.signal import find_peaks, peak_prominences
+        # peaks,_ = find_peaks(img_int_dist, width=5, rel_height=10)
+
+        # prominences, l, r = peak_prominences(img_int_dist, peaks) 
+        # h_peak = np.argmax(prominences)
+
+        # img_bw_compressed[img_bw_compressed < l[h_peak]] = 0
+        # img_bw_compressed[img_bw_compressed > r[h_peak]] = 0
+        # img_bw_compressed[img_bw_compressed != 0] = 255
+        # print()
+        
+        # # self.__plot_rgb_channel_cumulative(IMG)
+        # # self.__plot_rgb_ch_intensity_dist(IMG)
+        # self._img = IMG
+
+    # @staticmethod
+    # def __rgb_ch_intensity_dist(img_ch: np.ndarray, intensity_range: int = 1<<8) -> np.ndarray:
+    #     ret_val,_ = np.histogram(img_ch, bins=np.arange(intensity_range))
+    #     return ret_val
+        
+    # @staticmethod
+    # def __rgb_channel_cumulative(img_ch: np.ndarray) -> np.ndarray:
+    #     return np.asarray([
+    #         np.sum(img_ch[:ii+1]) for ii,_ in enumerate(img_ch)
+    #     ]) / np.sum(img_ch)
+
+    # @staticmethod
+    # def __plot_rgb_channel_cumulative(img: np.ndarray) -> None:
+    #     r_ch = img[:,:,1]
+    #     b_ch = img[:,:,0]
+    #     g_ch = img[:,:,2]
+    #     img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+    #     img_int_dist = Image.__rgb_ch_intensity_dist(img_bw)
+    #     b_int_dist = Image.__rgb_ch_intensity_dist(b_ch)
+    #     r_int_dist = Image.__rgb_ch_intensity_dist(r_ch)
+    #     g_int_dist = Image.__rgb_ch_intensity_dist(g_ch)
+        
+    #     img_cum = Image.__rgb_channel_cumulative(img_int_dist)
+    #     b_cum = Image.__rgb_channel_cumulative(b_int_dist)
+    #     r_cum = Image.__rgb_channel_cumulative(r_int_dist)
+    #     g_cum = Image.__rgb_channel_cumulative(g_int_dist)
+
+    #     plt.figure(figsize=(8,6))
+    #     plt.plot(img_cum, label="bw image intensity cumulative", color="y")
+    #     plt.plot(b_cum, label="blue channel intensity cumulative", color="b")
+    #     plt.plot(r_cum, label="red channel intensity cumulative", color="r")
+    #     plt.plot(g_cum, label="green channel intensity cumulative", color="g")
+    #     plt.legend()
+    #     plt.grid()
+    #     plt.xlabel("channel value")
+    #     plt.ylabel("N")
+    #     mpld3.show()
+
+    # @staticmethod
+    # def __plot_rgb_ch_intensity_dist(img: np.ndarray) -> None:
+    #     r_ch = img[:,:,1]
+    #     b_ch = img[:,:,0]
+    #     g_ch = img[:,:,2]
+    #     img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #     img_int_dist = Image.__rgb_ch_intensity_dist(img_bw)
+    #     b_int_dist = Image.__rgb_ch_intensity_dist(b_ch)
+    #     r_int_dist = Image.__rgb_ch_intensity_dist(r_ch)
+    #     g_int_dist = Image.__rgb_ch_intensity_dist(g_ch)
+
+    #     plt.figure(figsize=(8,6))
+    #     plt.plot(img_int_dist, label="bw image intensity dist", color="y")
+    #     plt.plot(r_int_dist, label="red channel intensity dist", color="r")
+    #     plt.plot(b_int_dist, label="blue channel intensity dist", color="b")
+    #     plt.plot(g_int_dist, label="green channel intensity dist", color="g")
+    #     plt.legend()
+    #     plt.grid()
+    #     plt.xlabel("channel value")
+    #     plt.ylabel("N")
+    #     mpld3.show()

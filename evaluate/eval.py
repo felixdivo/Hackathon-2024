c74cf37231a4a0faf9506e3de816521ec5edb2d1
Felix Divo <felix.divo@cs.tu-darmstadt.de>
2025-01-03 16:48:44 +0100

    Reformat

--- evaluate/eval.py
+++ evaluate/eval.py
@@ -8,10 +8,11 @@ import cvzone
 import cv2
 import numpy as np
 
+
 def overlay_gripper_on_part(gripper_image, part_image, x, y, angle):
     """
     Overlay the gripper image onto the part image with specified translation and rotation.
-    
+
     :param gripper_image: Input image of the gripper.
     :param part_image: Input image of the part mask.
     :param x: Horizontal offset (pixels) for gripper placement on the part.
@@ -20,7 +21,9 @@ def overlay_gripper_on_part(gripper_image, part_image, x, y, angle):
     :return: Combined image with the gripper overlayed on the part., Boolean flag indicating if the gripper intersects with background.
     """
     # Rotate the gripper image as first transformation step
-    rotated_gripper = cvzone.rotateImage(gripper_image, angle=angle, scale=1, keepSize=False)
+    rotated_gripper = cvzone.rotateImage(
+        gripper_image, angle=angle, scale=1, keepSize=False
+    )
 
     # Calculate offsets for translation step
     # Dimensions of the gripper and part images
@@ -28,52 +31,64 @@ def overlay_gripper_on_part(gripper_image, part_image, x, y, angle):
 
     # Pad the part image so that the gripper can be out of bounds, original is centered
     ph, pw = part_image.shape[:2]
-    pad_top, pad_bottom = gh,gh
+    pad_top, pad_bottom = gh, gh
     pad_left, pad_right = gw, gw
-    part_image_padded = np.ones((ph + (pad_top + pad_bottom), pw + (pad_left + pad_right), 3), dtype=np.uint8) * 255
-    part_image_padded[(pad_top):(pad_top + ph), (pad_left):(pad_left + pw)] = part_image
+    part_image_padded = (
+        np.ones(
+            (ph + (pad_top + pad_bottom), pw + (pad_left + pad_right), 3),
+            dtype=np.uint8,
+        )
+        * 255
+    )
+    part_image_padded[
+        (pad_top) : (pad_top + ph), (pad_left) : (pad_left + pw)
+    ] = part_image
 
     # apply tranformation offset
-    overlay_x = int(x - (gw/2) + pad_left)
-    overlay_y = int(y - (gh/2) + pad_top)
-    
+    overlay_x = int(x - (gw / 2) + pad_left)
+    overlay_y = int(y - (gh / 2) + pad_top)
+
     # Only for visualization
     part_image_copy = part_image_padded.copy()
-    mask_combined = cvzone.overlayPNG(part_image_copy, rotated_gripper, [overlay_x, overlay_y])
-
+    mask_combined = cvzone.overlayPNG(
+        part_image_copy, rotated_gripper, [overlay_x, overlay_y]
+    )
 
     ## detect intersections
 
     # make sure mask is 0,1 for instersection
     part_hsv = cv2.cvtColor(part_image_padded, cv2.COLOR_BGR2HSV)
     lower_white = np.array([0, 0, 20])
-    upper_white = np.array([180, 255, 255]) # HSV (0-180, 0-255, 0-255)
+    upper_white = np.array([180, 255, 255])  # HSV (0-180, 0-255, 0-255)
 
     part_hole_mask = cv2.inRange(part_hsv, lower_white, upper_white)
-   
 
     # Create a mask for the non-transparent regions of the gripper ,e.g the suction points
     alpha_channel = rotated_gripper[:, :, 3]  # Extract the alpha channel
-    gripper_mask = cv2.threshold(alpha_channel, 0, 255, cv2.THRESH_BINARY)[1]  # Binary mask for visible regions
+    gripper_mask = cv2.threshold(alpha_channel, 0, 255, cv2.THRESH_BINARY)[
+        1
+    ]  # Binary mask for visible regions
 
     # Align the visible mask with the part's coordinate system
     aligned_gripper_mask = np.zeros(part_image_padded.shape[:2], dtype=np.uint8)
-    aligned_gripper_mask[overlay_y:overlay_y + gh, overlay_x:overlay_x + gw] = gripper_mask
+    aligned_gripper_mask[
+        overlay_y : overlay_y + gh, overlay_x : overlay_x + gw
+    ] = gripper_mask
 
     # Check for intersection
     intersection = cv2.bitwise_and(aligned_gripper_mask, part_hole_mask)
     intersection_exists = np.any(intersection)
-    
-    if(intersection_exists):
+
+    if intersection_exists:
         print("Intersection detected!")
-    
+
     return mask_combined, intersection_exists
 
 
-def calculate_score(part_image, x, y, intersection = False):
+def calculate_score(part_image, x, y, intersection=False):
     """
     Calculate the score of the gripper placement on the part.
-    
+
     :param part_image: Input image of the part (background).
     :param x: Horizontal offset (pixels) for gripper placement on the part.
     :param y: Vertical offset (pixels) for gripper placement on the part.
@@ -85,9 +100,10 @@ def calculate_score(part_image, x, y, intersection = False):
         return np.inf
 
     ph, pw = part_image.shape[:2]
-    
+
     # Calculate the score
-    return np.sqrt((x-(pw/2))**2 + (y-(ph/2))**2)
+    return np.sqrt((x - (pw / 2)) ** 2 + (y - (ph / 2)) ** 2)
+
 
 def main():
     # Parse the command line arguments
@@ -132,21 +148,25 @@ def main():
         output_gripper = Path(output_row["gripper"])
         ground_truth_part = Path(ground_truth_row["part"])
         ground_truth_gripper = Path(ground_truth_row["gripper"])
-        if not (output_part == ground_truth_part and output_gripper == ground_truth_gripper):
+        if not (
+            output_part == ground_truth_part and output_gripper == ground_truth_gripper
+        ):
             print("Output and ground truth do not match!")
             print("Output: ----------")
             print(output_row)
             print("Ground truth: ----------")
             print(ground_truth_row)
-            continue ## end this iteration here
+            continue  ## end this iteration here
 
-        # Part matches!    
+        # Part matches!
 
         # Check constraint
         # load mask image from ground truth
         mask_image = cv2.imread(Path(ground_truth_row["mask"]), cv2.IMREAD_UNCHANGED)
         # load gripper image from ground truth
-        gripper_image = cv2.imread(Path(ground_truth_row["gripper"]), cv2.IMREAD_UNCHANGED)
+        gripper_image = cv2.imread(
+            Path(ground_truth_row["gripper"]), cv2.IMREAD_UNCHANGED
+        )
 
         if len(mask_image.shape) < 3:
             # convert to 3 channel image
@@ -156,9 +176,15 @@ def main():
         x_output = int(output_row["x"])
         y_output = int(output_row["y"])
         angle_output = int(output_row["angle"])
-        
+
         # Overlay gripper on part
-        res, intersect = overlay_gripper_on_part(gripper_image=gripper_image,part_image=mask_image, x=x_output, y=y_output, angle=angle_output)
+        res, intersect = overlay_gripper_on_part(
+            gripper_image=gripper_image,
+            part_image=mask_image,
+            x=x_output,
+            y=y_output,
+            angle=angle_output,
+        )
 
         # Display the resulting overlay, you may comment this out.
         if args.debug:
